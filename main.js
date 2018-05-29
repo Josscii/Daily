5793005cbe1a9f57d7b1c7fbcd5a1f29728d3929
chensheng <272169881@qq.com>
2018-05-29 18:57:46 +0800

    add statistics

--- main.js
+++ main.js
@@ -644,10 +644,7 @@ function initAppUI() {
                             } else if (indexPath.row == 3) {
                                 pushToNotifiList()
                             } else if (indexPath.row == 4) {
-                                $ui.alert({
-                                    title: "统计功能稍后上线",
-                                    message: "",
-                                })
+                                pushToStatistics()
                             }
                         } else {
                             
@@ -1409,6 +1406,68 @@ function scheduleNotification(notifi) {
 function cancelNotification(notifi) {
     $push.cancel({
         id: notifi.id
+    })
+}
+
+//-------------------------------- 统计
+
+function pushToStatistics() {
+    const file = $file.read("statistics.html")
+
+    $ui.push({
+        props: {
+            title: " 统计"
+        },
+        views: [
+            {
+                type: "web",
+                props: {
+                    get html() {
+                        return file.string
+                    },
+                    script: function() {
+                        var ctx = document.getElementById("myChart").getContext('2d');
+                        var myChart = new Chart(ctx, {
+                            type: 'line',
+                            data: {
+                                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                                datasets: [{
+                                    label: '# of Votes',
+                                    data: ["12 ml", "12 ml", "12 ml", "12 ml", "12 ml", "12 ml"],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(255,99,132,1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)'
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero:true
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                    }
+                },
+                layout: $layout.fill
+            }
+        ]
     })
 }
 
